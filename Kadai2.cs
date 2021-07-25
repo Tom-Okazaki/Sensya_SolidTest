151aed3d9fa7b1d660e0aa4fccc0be2b274e653d
岡崎　渡睦 <okazakitomu@OkazakiTomunomacbook.local>
2021-07-25 18:09:30 +0900

    add interface

--- Kadai2.cs
+++ Kadai2.cs
@@ -3,26 +3,36 @@ using System;
 namespace KADAI2
 {
     public class Kadai2{
+
+        interface IEnemies{
+            string Name{ get; set; }
+            int HP { get; set; }
+        }
+        interface IBullets{
+            string Name { get; set; }
+            int AttackPoint { get; set; }
+        }
+        interface IHIT{
+            int HIT(string Name, int HP, int AttackPoint);
+        }
         public class Player{
-            public class Bullet{
+            public class Bullet : IBullets{
                 public string Name { get; set; }
                 public int AttackPoint { get; set; }
                 public Bullet(string name, int attackpoint){
                     this.Name = name;
                     this.AttackPoint = attackpoint;
                 }
-                public class BulletHit{
+                public class BulletHit : IHIT{
                     public int HIT(string Name,int HP,int AttackPoint){
-                    int x;
-                    x = HP - AttackPoint;
+                    HP -= AttackPoint;
                     Console.WriteLine("プレイヤーの弾が{0}に当たりました",Name);
-                    return x;
+                    return HP;
                     }
                 }
             }//Bullet
         }//Player
-
-        public class Enemy{
+        public class Enemy : IEnemies{
             public string Name{ get; set; }
             public int HP { get; set; }
 
@@ -32,7 +42,7 @@ namespace KADAI2
             }
 
         }
-        public class Boss{
+        public class Boss : IEnemies{
             public string Name{ get; set; }
             public int HP { get; set; }
 
@@ -40,23 +50,24 @@ namespace KADAI2
                 this.Name = name;
                 this.HP = hp;
             }
-
         }
-        public static void Main(){
-            Player.Bullet B1 = new Player.Bullet("普通の弾",25);
-            Enemy E1 = new Enemy("エネミー１",200);
-            Boss BOS1 = new Boss("１面のボス", 500);
-            Kadai2.Player.Bullet.BulletHit BH = new Kadai2.Player.Bullet.BulletHit();
+        public class Test{
+            public static void Main(){
+            IBullets B1 = new Player.Bullet("普通の弾",25);
+            IEnemies E1 = new Enemy("エネミー１",200);
+            IEnemies BOS1 = new Boss("１面のボス", 500);
+            IHIT IHIT = new Kadai2.Player.Bullet.BulletHit();
 
             Console.WriteLine("{0}のHPは{1}, 弾の攻撃力は{2}",E1.Name,E1.HP,B1.AttackPoint);
-            E1.HP = BH.HIT(E1.Name,E1.HP,B1.AttackPoint);
+            E1.HP = IHIT.HIT(E1.Name,E1.HP,B1.AttackPoint);
             Console.WriteLine("{0}の残りHPは{1}",E1.Name ,E1.HP);
             
             Console.WriteLine("{0}のHPは{1}, 弾の攻撃力は{2}",BOS1.Name,BOS1.HP,B1.AttackPoint);
-            BOS1.HP = BH.HIT(BOS1.Name,BOS1.HP,B1.AttackPoint);
+            BOS1.HP = IHIT.HIT(BOS1.Name,BOS1.HP,B1.AttackPoint);
             Console.WriteLine("{0}の残りHPは{1}",BOS1.Name ,BOS1.HP);
-            BOS1.HP = BH.HIT(BOS1.Name,BOS1.HP,B1.AttackPoint);
+            BOS1.HP = IHIT.HIT(BOS1.Name,BOS1.HP,B1.AttackPoint);
             Console.WriteLine("{0}の残りHPは{1}",BOS1.Name ,BOS1.HP);
+            }
         }
     }
 }
